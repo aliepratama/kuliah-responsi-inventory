a2addd43e8a5baeff169d151fe2cbd6e0d852e7e
Alie Pratama <102345023+aliepratama@users.noreply.github.com>
2023-11-16 06:21:52 +0700

    fix: cors problem with flask

--- inventory-fe/src/routes/Products.vue
+++ inventory-fe/src/routes/Products.vue
@@ -1,10 +1,26 @@
 <script>
+import axios from 'axios';
+export default {
+    data(){
+        return {
+            datalists: 'asdasdasd',
+        }
+    },
+    mounted(){
+        axios.get('http://127.0.0.1:5000/products/lists')
+        .then(res => {
+            console.log(res);
+            this.datalists = res.data;
+        })
+    }
+}
 </script>
 <template>
     <div class="divide-gray-200 dark:divide-gray-700 min-w-full px-16">
         <div class="flex justify-between py-10">
             <div>
                 <h2 class="font-bold text-lg">Daftar Produk</h2>
+                <p>{{ datalists }}</p>
             </div>
             <div>
                 <router-link to="/form" type="button" class="px-4 py-2 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:bg-blue-100 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:bg-blue-800/30 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
