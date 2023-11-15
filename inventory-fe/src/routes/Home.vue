ed25452e2afafc910e946a77b4e0ea8e7d56f5d8
Alie Pratama <102345023+aliepratama@users.noreply.github.com>
2023-11-15 14:42:45 +0700

    feat: add home page

--- inventory-fe/src/routes/Home.vue
+++ inventory-fe/src/routes/Home.vue
@@ -1,5 +1,11 @@
 <script>
 </script>
 <template>
-    <h2>Home</h2>
+    <div class="min-w-full mt-40 flex flex-col items-center">
+        <h2 class="text-3xl font-extrabold">Inventory Management App</h2>
+        <p class="mt-4 text-lg font-medium">Kelola produk anda di aplikasi ini</p>
+        <router-link to="/products" type="button" class="mt-8 px-4 py-2 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:bg-blue-100 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:bg-blue-800/30 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+            Mulai
+        </router-link>
+    </div>
 </template>
