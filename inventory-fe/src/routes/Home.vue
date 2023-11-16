ffa8c67622bce1f98e59c5a94b0a1af396f733ca
Alie Pratama <102345023+aliepratama@users.noreply.github.com>
2023-11-16 13:06:57 +0700

    feat: make hero section on home view

--- inventory-fe/src/routes/Home.vue
+++ inventory-fe/src/routes/Home.vue
@@ -1,11 +1,19 @@
 <script>
 </script>
 <template>
-    <div class="min-w-full mt-40 flex flex-col items-center">
-        <h2 class="text-3xl font-extrabold">Inventory Management App</h2>
-        <p class="mt-4 text-lg font-medium">Kelola produk anda di aplikasi ini</p>
-        <router-link to="/products" type="button" class="mt-8 px-4 py-2 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:bg-blue-100 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:bg-blue-800/30 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-            Mulai
-        </router-link>
+    <div class="min-w-full mt-4 flex flex-col items-center">
+        <img 
+            src="../assets/home_4x.gif"
+            class="w-[40%] hidden md:block"
+            alt="inventory management">
+        <div class="min-w-full px-36 mt-16 md:mt-0 flex flex-col items-center md:flex-row md:justify-between">
+            <div class="flex flex-col items-center md:items-start">
+                <h2 class="text-3xl font-extrabold text-center md:text-left">Inventory Management App</h2>
+                <p class="mt-4 text-lg font-medium text-center md:text-left">Kelola produk anda di aplikasi ini</p>
+            </div>
+            <router-link to="/products" type="button" class="py-2 px-4 mt-16 md:mt-0 max-w-[5rem] text-center inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                Mulai
+            </router-link>
+        </div>
     </div>
 </template>
